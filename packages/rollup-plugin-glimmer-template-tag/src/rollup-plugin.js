ea5ec78e61b2634c85d978d9b9478408a21791d9
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-03-14 14:11:50 -0400

    Add peers

--- packages/rollup-plugin-glimmer-template-tag/src/rollup-plugin.js
+++ packages/rollup-plugin-glimmer-template-tag/src/rollup-plugin.js
@@ -1,18 +1,20 @@
 // @ts-check
-import fs from "node:fs/promises";
-import path from "node:path";
-import { preprocessEmbeddedTemplates } from "./preprocess-embedded-templates";
-import { TEMPLATE_TAG_NAME, TEMPLATE_TAG_PLACEHOLDER } from "./util";
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
-const FCCT_EXTENSION = /\.g([jt]s)$/;
+import { preprocessEmbeddedTemplates } from './preprocess-embedded-templates';
+import { TEMPLATE_TAG_NAME, TEMPLATE_TAG_PLACEHOLDER } from './util';
+
+const PLUGIN_KEY = 'glimmer-template-tag';
+const RELEVANT_EXTENSION_REGEX = /\.g([jt]s)$/;
 
 export default function firstClassComponentTemplates() {
   return {
-    name: "preprocess-fccts",
+    name: 'preprocess-glimmer-template-tag',
     async resolveId(source, importer, options) {
-      if (source.endsWith(".hbs")) return;
+      if (source.endsWith('.hbs')) return;
 
-      for (let ext of ["", ".gjs", ".gts"]) {
+      for (let ext of ['', '.gjs', '.gts']) {
         let result = await this.resolve(source + ext, importer, {
           ...options,
           skipSelf: true,
@@ -22,20 +24,20 @@ export default function firstClassComponentTemplates() {
           return;
         }
 
-        if (FCCT_EXTENSION.test(result?.id)) {
+        if (RELEVANT_EXTENSION_REGEX.test(result?.id)) {
           return resolutionFor(result.id);
         }
       }
     },
 
     async load(id) {
-      let originalId = this.getModuleInfo(id)?.meta?.fccts?.originalId ?? id;
+      let originalId = this.getModuleInfo(id)?.meta?.[PLUGIN_KEY]?.originalId ?? id;
 
       if (originalId !== id) {
         this.addWatchFile(originalId);
       }
 
-      if (FCCT_EXTENSION.test(originalId)) {
+      if (RELEVANT_EXTENSION_REGEX.test(originalId)) {
         return await preprocessTemplates(originalId);
       }
     },
@@ -44,23 +46,23 @@ export default function firstClassComponentTemplates() {
 
 function resolutionFor(originalId) {
   return {
-    id: originalId.replace(FCCT_EXTENSION, ".$1"),
+    id: originalId.replace(RELEVANT_EXTENSION_REGEX, '.$1'),
     meta: {
-      fccts: { originalId },
+      [PLUGIN_KEY]: { originalId },
     },
   };
 }
 
 async function preprocessTemplates(id) {
-  let ember = (await import("ember-source")).default;
-  let contents = await fs.readFile(id, "utf-8");
+  let ember = (await import('ember-source')).default;
+  let contents = await fs.readFile(id, 'utf-8');
 
   // This is basically taken directly from `ember-template-imports`
   let result = preprocessEmbeddedTemplates(contents, {
-    relativePath: path.relative(".", id),
+    relativePath: path.relative('.', id),
 
     getTemplateLocalsRequirePath: ember.absolutePaths.templateCompiler,
-    getTemplateLocalsExportPath: "_GlimmerSyntax.getTemplateLocals",
+    getTemplateLocalsExportPath: '_GlimmerSyntax.getTemplateLocals',
 
     templateTag: TEMPLATE_TAG_NAME,
     templateTagReplacement: TEMPLATE_TAG_PLACEHOLDER,
